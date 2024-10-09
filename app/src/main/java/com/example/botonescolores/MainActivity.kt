7535d2de5098610e7ebef6287e193c87510ff4a5
Adriamdam1 <aabeijoncarbajo@danielcastelao.org>
2024-10-09 12:44:25 +0200

    9-10-24

--- app/src/main/java/com/example/botonescolores/MainActivity.kt
+++ app/src/main/java/com/example/botonescolores/MainActivity.kt
@@ -1,34 +1,32 @@
 package com.example.botonescolores
 
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.botonescolores.ui.theme.BotonesColoresTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,8 +38,94 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//enum class para usar en los botones, TODO : aplicar a todos los componentes
+enum class ColoresNumerados(val colorNumero: Int, val nombre: String = "blanco") {
+    AZUL(1, "Azulito"),Rojo(2),Amarillo(3),Verde(4)
+}
+
 
 @Composable
+fun Botones() {
+    // Estado para el color y el nombre del color
+    var colorName by remember { mutableStateOf("") }
+    var colorValue by remember { mutableStateOf(Color.White) }
+
+    //var listaColoresPresionados = remember { mutableStateListOf<String>()}
+
+    // TODO clase mutable colores en proceso
+    //val entrees: MutableList<String> = mutableListOf()
+    //${entrees.add("hola")}
+
+    Column(modifier = Modifier.padding(vertical = 200.dp)) {
+        Button(
+            onClick = {
+                colorName = ColoresNumerados.AZUL.nombre
+                colorValue = Color.Blue
+                Log.d("Boton pulsado", "se ha pulsado el boton $colorName")
+
+
+
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+        ) {
+            Text("AZUL", fontSize = 24.sp, color = Color.White)
+        }
+
+        Button(
+            onClick = {
+                colorName = "Rojo"
+                colorValue = Color.Red
+                Log.d("Boton pulsado", "se ha pulsado el boton $colorName")
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+        ) {
+            Text("ROJO", fontSize = 24.sp, color = Color.White)
+        }
+
+        Button(
+            onClick = {
+                colorName = "Amarillo"
+                colorValue = Color.Yellow
+                Log.d("Boton pulsado", "se ha pulsado el boton $colorName")
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow)
+        ) {
+            Text("AMARILLO", fontSize = 24.sp, color = Color.Black)
+        }
+
+        Button(
+            onClick = {
+                colorName = "Verde"
+                colorValue = Color.Green
+                Log.d("Boton pulsado", "se ha pulsado el boton $colorName")
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
+        ) {
+            Text("VERDE", fontSize = 24.sp, color = Color.White)
+        }
+
+        // Campo de texto que muestra el nombre del color
+        TextField(
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxSize(), //rellena el maximo posible
+                //.fillMaxWidth(), //rellena horizontalmente
+            value = colorName, // Muestra el nombre del color
+            onValueChange = {},
+            label = { Text(text = "Color: ", fontSize = 24.sp) }, //Texto del titulo del textflied
+            textStyle = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold), // Texto del TextField
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = colorValue,
+                unfocusedContainerColor = colorValue,
+                disabledContainerColor = colorValue
+            )
+        )
+    }
+}
+
+
+/*
+     @Composable
 fun Botones(){
 
     var color = remember { mutableStateOf("") }
@@ -78,3 +162,4 @@ fun Botones(){
         //modifier = Modifier.
     }
 }
+        */
